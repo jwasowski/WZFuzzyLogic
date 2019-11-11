80f614dc92f605cd1cb90ae3df91d117b051bef8
jwasowski <jakub.wasowski@gmail.com>
2019-11-11 21:37:11 +0100

    Deleted "ninja" skill level, Modified FCL Rules Block, Added comments to Main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,35 +9,37 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Load from 'FCL' file
-        String fileName = "fcl/JWGZfcl.fcl";
-        FIS fis = FIS.load(fileName,true);
+		String fileName = "fcl/JWGZfcl.fcl";
+		FIS fis = FIS.load(fileName, true);
 
-        // Error while loading?
-        if( fis == null ) { 
-            System.err.println("Can't load file: '" + fileName + "'");
-            return;
-        }
-        // Get default function block
-     	FunctionBlock functionBlock = fis.getFunctionBlock(null);
-        
-        // Show 
-        JFuzzyChart.get().chart(functionBlock);
-        
-        
-        // Set inputs
-        fis.setVariable("skill", 2);
-        fis.setVariable("expirience", 7);
-        fis.setVariable("softSkills", 7);
+		// Error while loading?
+		if (fis == null) {
+			System.err.println("Can't load file: '" + fileName + "'");
+			return;
+		}
+		// Get default function block
+		FunctionBlock functionBlock = fis.getFunctionBlock(null);
 
-        // Evaluate
-        fis.evaluate();
+		// Show
+		JFuzzyChart.get().chart(functionBlock);
 
-        // Show output variable's chart
-        Variable salary = functionBlock.getVariable("salary");
-        JFuzzyChart.get().chart(salary, salary.getDefuzzifier(), true);
+		// Skill rating based on job requirements
+		fis.setVariable("skill", 1.9f);
+		// Years of Experience
+		fis.setVariable("experience", 1.1f);
+		// How good communicator he is?
+		fis.setVariable("softSkills", 2.1f);
 
-        // Print ruleSet
-        System.out.println(fis);
+		// Evaluate
+		fis.evaluate();
+
+		// Show output variable's chart
+		// Salary value should be multiplied by 1k 
+		Variable salary = functionBlock.getVariable("salary");
+		JFuzzyChart.get().chart(salary, salary.getDefuzzifier(), true);
+
+		// Print ruleSet
+		System.out.println(fis);
 
 	}
 
