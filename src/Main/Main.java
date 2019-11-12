086548bc32a368440dd28fab1ebb95c88dd5681c
jwasowski <jakub.wasowski@gmail.com>
2019-11-12 20:35:44 +0100

    Updates for handling CLI

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,6 +10,14 @@ public class Main {
 	public static void main(String[] args) {
 		// Load from 'FCL' file
 		String fileName = "fcl/JWGZfcl.fcl";
+		// Number of arguments checker
+		if (args.length < 3 || args.length > 3) {
+			System.err.println("Input must be of three arguments!");
+			System.err.println("Syntax: java -jar <jar name> <Skill value> <Experience value> <Soft Skills value>");
+			System.err.println("Example: java -jar testJar.jar 1.5 2.3 3.4");
+			System.exit(0);
+		}
+		String[] params = args;
 		FIS fis = FIS.load(fileName, true);
 
 		// Error while loading?
@@ -18,28 +26,32 @@ public class Main {
 			return;
 		}
 		// Get default function block
-		FunctionBlock functionBlock = fis.getFunctionBlock(null);
+		FunctionBlock functionBlock = fis.getFunctionBlock("salaryRating");
 
 		// Show
 		JFuzzyChart.get().chart(functionBlock);
 
 		// Skill rating based on job requirements
-		fis.setVariable("skill", 1.9f);
+		fis.setVariable("skill", Float.parseFloat(params[0]));
 		// Years of Experience
-		fis.setVariable("experience", 1.1f);
+		fis.setVariable("experience", Float.parseFloat(params[1]));
 		// How good communicator he is?
-		fis.setVariable("softSkills", 2.1f);
+		fis.setVariable("softSkills", Float.parseFloat(params[2]));
 
 		// Evaluate
 		fis.evaluate();
 
 		// Show output variable's chart
-		// Salary value should be multiplied by 1k 
+		// Salary value should be multiplied by 1k
 		Variable salary = functionBlock.getVariable("salary");
 		JFuzzyChart.get().chart(salary, salary.getDefuzzifier(), true);
 
 		// Print ruleSet
 		System.out.println(fis);
+		System.out.println("Skill value: " + params[0]);
+		System.out.println("Experience value: " + params[1]);
+		System.out.println("Soft Skills value: " + params[2]);
+		System.out.println("Estimated salary: " + Math.floor(salary.getValue() * 1000));
 
 	}
 
